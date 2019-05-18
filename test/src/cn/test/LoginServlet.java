29412d5b816775adbc6c8d8d420b8997bbec3048
GitwuweiHub <hainy_love@sina.com>
2019-05-18 15:15:17 +0800

    test项目首次提交

---
+++ test/src/cn/test/LoginServlet.java
@@ -0,0 +1,36 @@
+package cn.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
+
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=utf-8");
+		request.setCharacterEncoding("utf-8");
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		System.out.println("username="+username+",password="+password);
+		PrintWriter out = response.getWriter();
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("result", "ok");
+		out.print(jsonObject.toString());
+		out.flush();
+		out.close();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
